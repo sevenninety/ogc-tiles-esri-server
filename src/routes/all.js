d403cffcca06804a7642dd27ab26cea666732702
sevenninety <sevenninety@hotmail.co.uk>
2019-06-20 19:16:59 +0100

    Hook swagger

--- src/routes/all.js
+++ src/routes/all.js
@@ -11,6 +11,9 @@ const tiles = require("./tiles");
 const collections = require("./collections");
 const landingPage = require("./landingPage");
 
+// Documentation libraries
+const swaggerDocument = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname + "../../../openapi.yaml"), "utf8"));
+
 router.use("/", landingPage);
 
 // Add path for swagger documentation
