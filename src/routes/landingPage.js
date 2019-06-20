03544764234beef7e396bd6824b8e145c7ee3131
Adam Branscomb <abranscomb@esriuk.com>
2019-06-20 20:38:46 +0100

    sorted out landing page

--- src/routes/landingPage.js
+++ src/routes/landingPage.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 
 //var app = express();
 router.get("/", (req, res) => {
-    res.redirect("static/landingPage.html");
+    //res.redirect("/src/static/landingPage.html");
+    //res.set('Content-Type', 'text/html');
+    res.sendfile('landingPage.html', { root: __dirname + "/../static" } );
 });
 
 //app.use('/', express.static('app', options));
