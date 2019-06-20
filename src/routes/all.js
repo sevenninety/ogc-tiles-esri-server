c44115c350023f899d4fe1931ca65ec53db2ea34
sevenninety <sevenninety@hotmail.co.uk>
2019-06-20 19:11:05 +0100

    Add swagger doc

--- src/routes/all.js
+++ src/routes/all.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("js-yaml");
+const path = require("path");
+const fs = require("fs");
 
 // Routes
 const conformance = require("./conformance");
@@ -8,6 +12,12 @@ const collections = require("./collections");
 const landingPage = require("./landingPage");
 
 router.use("/", landingPage);
+
+// Add path for swagger documentation
+router.use("/api", swaggerUi.serve);
+router.get("/api", swaggerUi.setup(swaggerDocument));
+
+// Add others
 router.use("/conformance", conformance);
 router.use("/collections", collections);
 router.use("/tileMatrixSets", tileMatrixSets);
